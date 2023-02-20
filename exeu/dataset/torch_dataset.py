5a28476226a3eb3fb96f3c2ddbd185698084347c
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-20 17:13:17 +0100

    experiment setup17

--- exeu/dataset/torch_dataset.py
+++ exeu/dataset/torch_dataset.py
@@ -10,10 +10,10 @@ class TorchDataset(Dataset):
         y = self.data[["rhoT", "phiT"]].values[start:stop]
         x = self.data[["rho0", "phi0", "rho1", "phi1", "quad", "poisson"]]
         np.random.seed(seed)
-        x.loc[x["quad"]] = (
+        x.loc[:, ["quad"]] = (
             x["quad"].values + np.random.uniform(low=-0.5, high=0.5, size=len(x))
         ) / 4
-        x.loc[x["poisson"]] = (
+        x.loc[:, ['poisson']] = (
             x["poisson"].values + np.random.uniform(low=-0.5, high=0.5, size=len(x))
         ) / 76
         x = x.values[start:stop]
@@ -22,7 +22,7 @@ class TorchDataset(Dataset):
         self.x = torch.from_numpy(x)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.x)
 
     def __getitem__(self, idx):
         return self.x[idx], self.y[idx]
