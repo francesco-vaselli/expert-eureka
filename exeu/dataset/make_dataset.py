b2d1d633fc100c72b01849189caa724f84b063b1
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-20 16:13:01 +0100

    experiment setup

---
+++ exeu/dataset/make_dataset.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+
+def make_dataset(seed: int = 0, N: int = 100000) -> pd.DataFrame:
+    """Make a dataset for training and testing."""
+
+    np.random.seed(seed)
+    N = N
+
+    x0 = np.random.normal(loc=0, scale=1, size=N)
+    y0 = np.random.normal(loc=0, scale=1, size=N)
+    x1 = np.random.normal(loc=0, scale=1, size=N)
+    y1 = np.random.normal(loc=0, scale=1, size=N)
+
+    rho0 = np.hypot(x0, y0)
+    rho1 = np.hypot(x1, y1)
+    phi0 = np.arctan2(y0, x0)
+    phi1 = np.arctan2(y1, x1)
+
+    rhoT = np.hypot(x0 + x1, y0 + y1)
+    phiT = np.arctan2(y0 + y1, x0 + x1)
+
+    quad = np.zeros(N)
+    quad[(0 < phiT) & (phiT < np.pi / 2)] = 0
+    quad[(np.pi / 2 < phiT) & (phiT < np.pi)] = 1
+    quad[(-np.pi < phiT) & (phiT < -np.pi / 2)] = 2
+    quad[(-np.pi / 2 < phiT) & (phiT < 0)] = 3
+
+    poisson = np.random.poisson(lam=10 * rhoT, size=N)
+
+    df = pd.DataFrame(
+        {
+            "x0": x0,
+            "y0": y0,
+            "x1": x1,
+            "y1": y1,
+            "rho0": rho0,
+            "phi0": phi0,
+            "rho1": rho1,
+            "phi1": phi1,
+            "rhoT": rhoT,
+            "phiT": phiT,
+            "quad": quad,
+            "poisson": poisson,
+        },
+        dtype=np.float32,
+    )
+
+    return df
+
+
+if __name__ == "__main__":
+    df = make_dataset(seed=0, N=100000)
+    print(df.poisson.describe())
+    df.to_csv("data.csv", index=False)
---
+++ exeu/experiments/experiment0.py
@@ -0,0 +1,133 @@
+# experiment 0 of our toy problem
+import torch
+
+import time
+from tensorboardX import SummaryWriter
+import sys
+import os
+
+from exeu.utils.validation import validate
+from exeu.dataset.torch_dataset import TorchDataset
+from exeu.model.modded_base_flow import FlowM
+from exeu.model.modded_nflows_init import (
+    PiecewiseRationalQuadraticCouplingTransformM,
+    MaskedPiecewiseRationalQuadraticAutoregressiveTransformM,
+    )
+from exeu.utils.train_funcs import train
+
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
+from nflows import transforms
+
+
+def create_linear_transform(param_dim):
+    """Create the composite linear transform PLU.
+    Arguments:
+        input_dim {int} -- dimension of the space
+    Returns:
+        Transform -- nde.Transform object
+    """
+
+    return transforms.CompositeTransform(
+        [
+            transforms.RandomPermutation(features=param_dim),
+            transforms.LULinear(param_dim, identity_init=True),
+        ]
+    )
+
+def trainer(tr_dataset, te_dataset, val_func):
+
+    args = get_args()
+    print(args)
+    args.log_name = "ex0"
+    save_dir = os.path.join("checkpoints", args.log_name)
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+
+    if args.log_name is not None:
+        log_dir = "runs/%s" % args.log_name
+    else:
+        log_dir = "runs/time-%d" % time.time()
+
+    writer = SummaryWriter(logdir=log_dir)
+    # save hparams to tensorboard
+    writer.add_hparams(vars(args), {})
+
+    # define model
+
+
+    if args.device == "cuda":  # Single process, single GPU per process
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+            model.to(device)
+            print("!!  USING GPU  !!")
+
+    else:
+        print("!!  USING CPU  !!")
+
+    # resume checkpoints
+    res_epoch = 0
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=args.lr,
+        betas=(args.beta1, args.beta2),
+        weight_decay=args.weight_decay,
+    )
+
+    if args.resume_checkpoint is None and os.path.exists(
+        os.path.join(save_dir, "checkpoint-latest.pt")
+    ):
+        args.resume_checkpoint = os.path.join(
+            save_dir, "checkpoint-latest.pt"
+        )  # use the latest checkpoint
+    if args.resume_checkpoint is not None and args.resume == True:
+        model, _, _, res_epoch, _, _ = load_model(
+            device,
+            model_dir=save_dir,
+            filename="checkpoint-latest.pt",
+        )
+        print(f"Resumed from: {res_epoch}")
+
+    train_loader = torch.utils.data.DataLoader(
+        dataset=tr_dataset,
+        batch_size=args.batch_size,
+        shuffle=~args.sorted_dataset,
+        num_workers=args.n_load_cores,
+        pin_memory=True,
+        sampler=None,
+        drop_last=True,
+        # worker_init_fn=init_np_seed,
+    )
+    if args.sorted_dataset:
+        print("train dataset NOT shuffled")
+
+    test_loader = torch.utils.data.DataLoader(
+        dataset=te_dataset,
+        batch_size=args.batch_size,  # manually set batch size to avoid diff shapes
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True,
+        drop_last=True,
+    )
+
+    # print total params number and stuff
+    total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    print(total_params)
+    print(len(train_loader.dataset))
+
+    trh, tsh = train(
+        model,
+        train_loader,
+        test_loader,
+        epochs=args.epochs,
+        optimizer=optimizer,
+        device=torch.device(args.device),
+        name="model",
+        model_dir=save_dir,
+        args=args,
+        writer=writer,
+        output_freq=100,
+        save_freq=args.save_freq,
+        res_epoch=res_epoch,
+        val_func=validate,
+    )
