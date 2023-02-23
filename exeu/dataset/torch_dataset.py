67d57b32fcae3d7e6e28b5d6974cfead8947a8fa
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-02-23 18:02:33 +0100

    load all to gpu

--- exeu/dataset/torch_dataset.py
+++ exeu/dataset/torch_dataset.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class TorchDataset(Dataset):
-    def __init__(self, csv_file, start=0, stop=None, seed=0):
+    def __init__(self, csv_file, start=0, stop=None, seed=0, gpu=0):
         self.data = pd.read_csv(csv_file)
         y = self.data[["rhoT", "phiT"]].values[start:stop]
         x = self.data[["rho0", "phi0", "rho1", "phi1", "quad", "poisson"]]
@@ -18,8 +18,8 @@ class TorchDataset(Dataset):
         ) / 16
         x = x.values[start:stop]
 
-        self.y = torch.from_numpy(y).float()
-        self.x = torch.from_numpy(x).float()
+        self.y = torch.from_numpy(y).float().cuda(gpu)
+        self.x = torch.from_numpy(x).float().cuda(gpu)
 
     def __len__(self):
         return len(self.x)
