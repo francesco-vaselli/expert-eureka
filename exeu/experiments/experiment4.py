e6122a663f7db0100e8e2949478aa24bdb595095
francesco-vaselli <francesco.vaselli@protonmail.com>
2023-03-22 17:30:24 +0100

    TORCH COMPILE timing5

--- exeu/experiments/experiment4.py
+++ exeu/experiments/experiment4.py
@@ -156,10 +156,11 @@ def trainer(tr_dataset, te_dataset, val_func):
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
     print(total_params)
     print(len(train_loader.dataset))
-    # model = torch.compile(model, mode='max-autotune', fullgraph=True)
+    modelc = torch.compile(model, mode='max-autotune', fullgraph=True)
+    print('compiled')
 
     trh, tsh = train(
-        model,
+        modelc,
         train_loader,
         test_loader,
         epochs=args.epochs,
